32e6ff0cb6b5ea070f0d8ff282ee3c6a8d5a0a95
doyou1 <tla149@daum.net>
2020-10-10 14:25:57 +0900

    1.4 Nweet, CRUD 기능 및 실시간 불러오기 기능 구현 완료

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,14 @@ import {authService} from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+  const [userObj, setUserObj] = useState(null);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user){
-        setIsLoggedIn(true);
-      }else{
+        setIsLoggedIn(true); 
+        setUserObj(user);
+     }else{
         setIsLoggedIn(false);
       }
       setInit(true);  
@@ -23,7 +25,11 @@ function App() {
 
   return(
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? (
+      <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>
+      ) : (
+        "Initializing..."
+      )}
     <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </>
   );
